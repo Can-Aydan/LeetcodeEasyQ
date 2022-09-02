ec9280f9d00f8b16c876c1cb177224f4d74fe2d4
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-02 15:08:44 +0300

    Add files via upload

---
+++ mergeStringsAlternately1768/Main.java
@@ -0,0 +1,11 @@
+package mergeStringsAlternately1768;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		String word1="abc",word2="pqr";
+		System.out.println(solution.mergeAlternately(word1, word2));
+	}
+
+}
