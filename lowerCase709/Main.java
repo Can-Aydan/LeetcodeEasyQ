1594b92f704fbaabde019bbe991a52f3622b25a7
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-05 11:04:12 +0300

    Add files via upload

---
+++ lowerCase709/Main.java
@@ -0,0 +1,11 @@
+package lowerCase709;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution solu = new Solution();
+		String s = "al&phaBET";
+		System.out.println(solu.toLowerCase(s));
+	}
+
+}
