3b12643316f32a0bd4a70a27f2791d67670b40a7
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-05 15:30:00 +0300

    Add files via upload

---
+++ validAnagram242/Main.java
@@ -0,0 +1,11 @@
+package validAnagram242;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution solu = new Solution();
+		String s = "rat", t = "car";
+		System.out.println(solu.isAnagram(s, t));
+	}
+
+}
