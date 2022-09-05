04ad9a1191886cc3e2c49429dcdde9091d772a99
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-05 14:08:39 +0300

    Add files via upload

---
+++ containsDuplicate217/Main.java
@@ -0,0 +1,11 @@
+package containsDuplicate217;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums = { 2,14,18,22,22 };
+		System.out.println(sol.containsDuplicate(nums));
+	}
+
+}
