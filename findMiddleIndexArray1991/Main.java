06c1716dbbca7c522c77e458941acc58b0679db8
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-08 13:57:40 +0300

    Add files via upload

---
+++ findMiddleIndexArray1991/Main.java
@@ -0,0 +1,12 @@
+package findMiddleIndexArray1991;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums = { 1, 7, 3, 6, 5, 6 };
+		System.out.println(sol.pivotIndex(nums));
+
+	}
+
+}
---
+++ findMiddleIndexArray1991/Solution.java
@@ -0,0 +1,18 @@
+package findMiddleIndexArray1991;
+
+public class Solution {
+	public int pivotIndex(int[] nums) {
+		int total=0,leftTotal=0;
+		for(int i:nums) {
+			total+=i;
+		}
+		for(int i=0;i<nums.length;i++) {
+			if(leftTotal==total-leftTotal-nums[i]) {
+				return i;
+			}
+			leftTotal+=nums[i];
+			
+		}
+		return -1;
+	}
+}
