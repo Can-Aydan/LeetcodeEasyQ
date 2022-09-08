d9b199edec4276d73a74ae45aaaf77887f4ded52
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-08 13:56:08 +0300

    Add files via upload

---
+++ findPivotIndex724/Main.java
@@ -0,0 +1,11 @@
+package findPivotIndex724;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums = { 1, 7, 3, 6, 5, 6 };
+		System.out.println(sol.pivotIndex(nums));
+	}
+
+}
