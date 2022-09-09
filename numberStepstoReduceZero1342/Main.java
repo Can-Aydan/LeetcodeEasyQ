7fb166c4422d6b69322c1da3a7bd5536b18152a5
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-09 14:02:43 +0300

    Add files via upload

---
+++ numberStepstoReduceZero1342/Main.java
@@ -0,0 +1,11 @@
+package numberStepstoReduceZero1342;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int num = 14;
+		System.out.println(solution.numberOfSteps(num));
+	}
+
+}
