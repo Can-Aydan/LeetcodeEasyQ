e0bb47f791414483da8e6dcc49d9c631b3069616
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-09 13:53:52 +0300

    Add files via upload

---
+++ removeDuplicatesFromSortedArray26/Main.java
@@ -0,0 +1,11 @@
+package removeDuplicatesFromSortedArray26;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		Solution solution = new Solution();
+		System.out.println(solution.removeDuplicates(nums));
+	}
+
+}
