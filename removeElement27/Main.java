bf5a07bd9b06018d77e755e689cfef25b7496ebc
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-09-09 13:57:07 +0300

    Add files via upload

---
+++ removeElement27/Main.java
@@ -0,0 +1,11 @@
+package removeElement27;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+		int val = 2;
+		Solution solution = new Solution();
+		System.out.println(solution.removeElement(nums, val));
+	}
+}
