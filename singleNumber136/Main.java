55eb90a8f7ae01654076f3b972e353bd752cbe96
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-10-05 15:40:22 +0300

    Add files via upload
    easy231

---
+++ singleNumber136/Main.java
@@ -0,0 +1,11 @@
+package singleNumber136;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] nums= {4,1,2,1,2};
+		System.out.println(sol.singleNumber(nums));
+	}
+
+}
---
+++ singleNumber136/Solution.java
@@ -0,0 +1,11 @@
+package singleNumber136;
+
+public class Solution {
+	public int singleNumber(int[] nums) {
+		int answer = 0;
+		for (int i : nums) {
+			answer ^= i;
+		}
+		return answer;
+	}
+}
