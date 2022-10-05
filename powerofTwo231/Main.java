a0b7c61144e8c8e96aefa71c97c42d5b78e3111f
mrTurtleCoder <112491094+mrTurtleCoder@users.noreply.github.com>
2022-10-05 15:51:21 +0300

    easy231

---
+++ powerofTwo231/Main.java
@@ -0,0 +1,11 @@
+package powerofTwo231;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int n = 16;
+		System.out.println(sol.isPowerOfTwo(n));
+	}
+
+}
